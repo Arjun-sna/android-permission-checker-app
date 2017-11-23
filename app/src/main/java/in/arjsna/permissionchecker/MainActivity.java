58496fe4d17b6e6fe03b979b76428864ac98e459
arjun <arj.sna@gmail.com>
2017-11-23 09:01:04 +0530

    Add app rating dialog

--- app/src/main/java/in/arjsna/permissionchecker/MainActivity.java
+++ app/src/main/java/in/arjsna/permissionchecker/MainActivity.java
@@ -2,10 +2,13 @@ package in.arjsna.permissionchecker;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import com.github.fernandodev.easyratingdialog.library.EasyRatingDialog;
 import in.arjsna.permissionchecker.basemvp.BaseActivity;
 import in.arjsna.permissionchecker.permissiongrouplist.PermissionListFragment;
 
 public class MainActivity extends BaseActivity {
+
+  private EasyRatingDialog ratingDialog;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -15,6 +18,18 @@ public class MainActivity extends BaseActivity {
           .add(R.id.permission_container, new PermissionListFragment())
           .commit();
     }
+    ratingDialog = new EasyRatingDialog(this);
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+    ratingDialog.onStart();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    ratingDialog.showIfNeeded();
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
